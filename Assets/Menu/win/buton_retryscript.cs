5b5fb799eacf571284ec862601af20090d12a37e
dadcopter <nickyoung15@hotmail.com>
2014-10-14 11:57:15 +1000

    end screen art
    Not exactly sure how to implement this?

---
+++ Assets/Menu/win/buton_retryscript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buton_retryscript : MonoBehaviour {
+	public Texture mouseover, mouseout;
+
+	void OnMouseDown(){
+		Debug.Log ("Retry button pressed");
+		Application.LoadLevel (1);
+	}
+	void OnMouseOver(){
+		Debug.Log ("Retry on");
+		GetComponent<GUITexture>().texture = mouseover;
+	}
+	void OnMouseExit(){
+		Debug.Log ("Retry off");
+		GetComponent<GUITexture>().texture = mouseout;
+	}
+}
